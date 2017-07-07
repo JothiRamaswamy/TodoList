efa382c3747ef3290f15f21e063f4916b8b0bd41
Jothi Ramaswamy <jothiramaswamy@thinksteam4girls.org>
2017-07-07 12:41:10 -0400

    first commit

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,11 +8,68 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+    @IBOutlet weak var table: UITableView!
+    
+    var items:[String] = []
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return items.count
+        
+    }
+    
+
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        }
+        
+        override func viewDidAppear(_ animated: Bool){
+            
+            let itemObject = UserDefaults.standard.object(forKey: "items")
+            
+            
+            if let tempItems = itemObject as? [String]{
+                
+                items =  tempItems
+            
+        }
+            
+            table.reloadData()
+
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            
+            
+            
+            items.remove(at: indexPath.row)
+            
+            table.reloadData()
+            
+            UserDefaults.standard.set(items, forKey: "items")
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,13 +8,46 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBOutlet weak var itemTextField: UITextField!
 
+    @IBAction func add(_ sender: Any) {
+        
+        let itemObject = UserDefaults.standard.object(forKey: "items")
+        
+        var items: [String]
+        
+        if let tempItems = itemObject as? [String]{
+            
+            items =  tempItems
+            
+            items.append(itemTextField.text!)
+            
+        } else {
+            
+            items = [itemTextField.text!]
+            
+        }
+        
+        UserDefaults.standard.set(items, forKey: "items")
+        
+        itemTextField.text = ""
+        itemTextField.resignFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
